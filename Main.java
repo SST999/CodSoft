f1d55fccf65b45a92acc25564dfb2057323f4485
SST999 <142376165+SST999@users.noreply.github.com>
2023-10-20 14:07:26 +0530

    Task 5

--- Main.java
+++ Main.java
@@ -1,86 +1,138 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class BankAccount {
-    private double balance;
+class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
 
-    public BankAccount(double initialBalance) {
-        balance = initialBalance;
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
     }
 
-    public double getBalance() {
-        return balance;
+    public int getRollNumber() {
+        return rollNumber;
     }
 
-    public void deposit(double amount) {
-        balance += amount;
-    }
-
-    public boolean withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            return true;
-        }
-        return false;
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
     }
 }
 
-class ATM {
-    private BankAccount account;
+class StudentManagementSystem {
+    private List<Student> students;
 
-    public ATM(BankAccount bankAccount) {
-        account = bankAccount;
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
     }
 
-    public void displayMenu() {
-        System.out.println("ATM Menu:");
-        System.out.println("1. Check Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
+    public void addStudent(Student student) {
+        students.add(student);
     }
 
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            displayMenu();
-            System.out.print("Select an option: ");
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Your balance: Rs. " + account.getBalance());
-                    break;
-                case 2:
-                    System.out.print("Enter amount to deposit: ");
-                    double depositAmount = scanner.nextDouble();
-                    account.deposit(depositAmount);
-                    System.out.println("Deposit successful. Your balance: Rs. " + account.getBalance());
-                    break;
-                case 3:
-                    System.out.print("Enter amount to withdraw: ");
-                    double withdrawAmount = scanner.nextDouble();
-                    if (account.withdraw(withdrawAmount)) {
-                        System.out.println("Withdrawal successful. Your balance: Rs. " + account.getBalance());
-                    } else {
-                        System.out.println("Insufficient balance.");
-                    }
-                    break;
-                case 4:
-                    System.out.println("Thank you for using the ATM!");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("Invalid option. Please select a valid option.");
+    public boolean removeStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                students.remove(student);
+                return true;
             }
         }
+        return false;
+    }
+
+    public Student searchStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> getAllStudents() {
+        return students;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount userAccount = new BankAccount(1000); // Initial balance
-        ATM atm = new ATM(userAccount);
-        atm.run();
+        Scanner scanner = new Scanner(System.in);
+        StudentManagementSystem sms = new StudentManagementSystem();
+
+        while (true) {
+            System.out.println("1. Add Student");
+            System.out.println("2. Remove Student");
+            System.out.println("3. Search Student");
+            System.out.println("4. Display All Students");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    scanner.nextLine(); // Consume the newline
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter roll number: ");
+                    int rollNumber = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline
+                    System.out.print("Enter grade: ");
+                    String grade = scanner.nextLine();
+
+                    Student newStudent = new Student(name, rollNumber, grade);
+                    sms.addStudent(newStudent);
+                    System.out.println("Student added.");
+                    break;
+
+                case 2:
+                    System.out.print("Enter roll number of student to remove: ");
+                    int rollToRemove = scanner.nextInt();
+                    boolean removed = sms.removeStudent(rollToRemove);
+                    if (removed) {
+                        System.out.println("Student removed.");
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter roll number of student to search: ");
+                    int rollToSearch = scanner.nextInt();
+                    Student searchedStudent = sms.searchStudent(rollToSearch);
+                    if (searchedStudent != null) {
+                        System.out.println("Student found:");
+                        System.out.println(searchedStudent);
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+
+                case 4:
+                    List<Student> allStudents = sms.getAllStudents();
+                    if (allStudents.isEmpty()) {
+                        System.out.println("No students to display.");
+                    } else {
+                        System.out.println("All Students:");
+                        for (Student student : allStudents) {
+                            System.out.println(student);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please choose again.");
+                    break;
+            }
+        }
     }
 }
